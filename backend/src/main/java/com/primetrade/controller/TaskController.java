0bf8ba8f703141fd5ee68534ae1f25c17e404025
@rakeshkandar2021 <rakeshkandar@gmail.com>
2026-03-29 14:23:34 +0530

    add api endpoint Security

--- backend/src/main/java/com/primetrade/controller/TaskController.java
+++ backend/src/main/java/com/primetrade/controller/TaskController.java
@@ -8,33 +8,34 @@ import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("/tasks")
+@RequestMapping("/tasks/v1")
 @CrossOrigin("http://localhost:5173/**")
 public class TaskController {
     @Autowired
     private TaskService taskService;
-
+    @PreAuthorize("hasRole('ADMIN')")
     @PostMapping
     public ResponseEntity<Task> create(@Valid @RequestBody TaskRequest request) {
         return new ResponseEntity<>(taskService.create(request), HttpStatus.CREATED);
     }
-
+    @PreAuthorize("hasAnyRole('USER','ADMIN')")
     @GetMapping
     public ResponseEntity<List<Task>> getAll() {
         return new ResponseEntity<>(taskService.getAll(), HttpStatus.OK);
     }
-
+    @PreAuthorize("hasAnyRole('ADMIN')")
     @PutMapping("/{id}")
     public ResponseEntity<Task> update(@PathVariable Long id,
                        @Valid @RequestBody TaskRequest request) {
         return new ResponseEntity<>(taskService.update(id, request), HttpStatus.OK);
     }
-
+    @PreAuthorize("hasRole('ADMIN')")
     @DeleteMapping("/{id}")
     public ResponseEntity<String> delete(@PathVariable Long id) {
        return new ResponseEntity<>(taskService.delete(id), HttpStatus.OK);
